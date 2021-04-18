7d38b4a07d22663fd604c76f2f465ee20043dc70
guilhermebigois <guilhermebigois@outlook.com>
2021-04-17 23:23:19 -0300

    Added environment variables to change Protheus database parameters

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,20 @@
 # Variavéis do Dockerfile PostgreSQL
-ARG POSTGRES_VERSION=13.2
+ARG POSTGRES_VERSION=latest
 
 # Origem e mantenedor
 FROM postgres:${POSTGRES_VERSION}
 LABEL maintainer="Guilherme Bigois"
 
-# Variáveis dinâmicas da base de dados Protheus
-ARG PROTHEUS_DBUSER
-ARG PROTHEUS_DBPASSWORD
-ARG PROTHEUS_DBNAME
-
-# Variáveis de ambiente da base de dados Protheus
-ENV INTERNAL_PROTHEUS_DBUSER=${PROTHEUS_DBUSER:-protheus}
-ENV INTERNAL_PROTHEUS_DBPASSWORD=${PROTHEUS_DBPASSWORD:-protheus}
-ENV INTERNAL_PROTHEUS_DBNAME=${PROTHEUS_DBNAME:-protheus}
+RUN apt-get update
+RUN apt install dumb-init
 
 # Cria o arquivo de inicialização
+COPY init.sh /
 COPY init.sql /docker-entrypoint-initdb.d/
-RUN sed -i 's/{{PROTHEUS_DBUSER}}/'"${INTERNAL_PROTHEUS_DBUSER}"'/' /docker-entrypoint-initdb.d/init.sql
-RUN sed -i 's/{{PROTHEUS_DBPASSWORD}}/'"${INTERNAL_PROTHEUS_DBPASSWORD}"'/' /docker-entrypoint-initdb.d/init.sql
-RUN sed -i 's/{{PROTHEUS_DBNAME}}/'"${INTERNAL_PROTHEUS_DBNAME}"'/' /docker-entrypoint-initdb.d/init.sql
+
+# Inicia o script my-init.sh antes do 
+ENTRYPOINT ["/usr/bin/dumb-init", "--"]
+CMD ["/init.sh"]
 
 # Definição do diretório inicial
 WORKDIR /var/lib/postgresql
---
+++ init.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Mensagem de boas vindas
+echo "*-------------------------------------------------------*"
+echo "|       Welcome to PostgreSQL for TOTVS Protheus        |"
+echo "*-------------------------------------------------------*"
+echo "| Created by: Guilherme Bigois                          |"
+echo "| LinkedIn: https://www.linkedin.com/in/guilhermebigois |"
+echo "| GitHub: https://github.com/guilhermebigois            |"          
+echo "*-------------------------------------------------------*"
+
+# Iniciando o processo de substitução das variáveis
+echo "[INFO][$(date '+%Y-%m-%d %H:%M:%S')][Setting Protheus database values...]"
+
+# Verifica se a variável de ambiente PROTHEUS_DBUSER foi configurada
+# Caso sim, utiliza para alterar o arquivo init.sql em /docker-entrypoint-initdb.d/
+DB_USER=$(printenv PROTHEUS_DBUSER)
+
+if [ -z "${DB_USER}" ]; then
+    echo "[WARN][$(date '+%Y-%m-%d %H:%M:%S')][Enviroment variable PROTHEUS_DBUSER is empty.]"
+    echo "[INFO][$(date '+%Y-%m-%d %H:%M:%S')][Using default PROTHEUS_DBUSER value: protheus]"
+else
+    echo "[WARN][$(date '+%Y-%m-%d %H:%M:%S')][Enviroment variable PROTHEUS_DBUSER is not empty.]"
+    echo "[INFO][$(date '+%Y-%m-%d %H:%M:%S')][Using ${DB_USER} for Protheus database user]"
+fi
+
+sed -i 's/{{PROTHEUS_DBUSER}}/'"${DB_USER}"'/' /docker-entrypoint-initdb.d/init.sql
+
+# Verifica se a variável de ambiente PROTHEUS_DBPASSWORD foi configurada
+# Caso sim, utiliza para alterar o arquivo init.sql em /docker-entrypoint-initdb.d/
+DB_PASS=$(printenv PROTHEUS_DBPASSWORD)
+
+if [ -z "${DB_PASS}" ]; then
+    echo "[WARN][$(date '+%Y-%m-%d %H:%M:%S')][Enviroment variable PROTHEUS_DBPASSWORD is empty.]"
+	echo "[INFO][$(date '+%Y-%m-%d %H:%M:%S')][Using default PROTHEUS_DBPASSWORD value: protheus]"
+else
+    echo "[WARN][$(date '+%Y-%m-%d %H:%M:%S')][Enviroment variable PROTHEUS_DBPASSWORD is not empty.]"
+	echo "[INFO][$(date '+%Y-%m-%d %H:%M:%S')][Using ${DB_PASS} for Protheus database user password]"
+fi
+
+sed -i 's/{{PROTHEUS_DBPASSWORD}}/'"${DB_PASS}"'/' /docker-entrypoint-initdb.d/init.sql
+
+# Verifica se a variável de ambiente PROTHEUS_DBNAME foi configurada
+# Caso sim, utiliza para alterar o arquivo init.sql em /docker-entrypoint-initdb.d/
+DB_NAME=$(printenv PROTHEUS_DBNAME)
+
+if [ -z "${DB_NAME}" ]; then
+    echo "[WARN][$(date '+%Y-%m-%d %H:%M:%S')][Enviroment variable PROTHEUS_DBNAME is empty.]"
+	echo "[INFO][$(date '+%Y-%m-%d %H:%M:%S')][Using default PROTHEUS_DBNAME value: protheus]"
+else
+    echo "[WARN][$(date '+%Y-%m-%d %H:%M:%S')][Enviroment variable PROTHEUS_DBNAME is not empty.]"
+	echo "[INFO][$(date '+%Y-%m-%d %H:%M:%S')][Using ${DB_NAME} for Protheus database name]"
+fi
+
+# Inicia o banco de dados
+exec /docker-entrypoint.sh postgres
